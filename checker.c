ebc8750b86f32903bb54fd9d3ee9fbf1ccf368be
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-02-08 21:04:18 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -5,7 +5,8 @@
 #define ALL_OK		0
 #define ALL_NOT_OK	1
 
-char ParameterNames[3]={"Temperature","SOC","Charge Rate"};
+char ParameterNames[3];
+ParameterNames[]={"Temperature","SOC","Charge Rate"};
 
 typedef enum {
   TempParameter,
