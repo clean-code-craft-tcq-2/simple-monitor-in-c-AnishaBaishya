8f2e9c8574130b2421d7e2186d420fcd8a74ced4
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-02-08 22:17:42 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -124,7 +124,7 @@ int main() {
   setRangeValues("Charge Rate",0.0,0.8);
 	
   float TestParameters1[]={25, 70, 0.7};
-	printf(TestParameters1);
+  printf("hello %f",TestParameters1);
   TestBatteryIsOk(ALL_OK,TestParameters1);
 	
 //   float TestParameters2[]={50, 85, 0};
