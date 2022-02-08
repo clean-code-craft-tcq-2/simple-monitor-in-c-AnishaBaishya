ace570617468c2ccdbbf20771bf068f3b10e7dcd
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-02-08 10:17:39 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdbool.h>
 
 #define MINIMUM_TEMPERATURE    0.0
 #define MAXIMUM_TEMPERATURE    45.0
@@ -9,35 +10,35 @@
 #define MAXIMUM_CHARGERATE    0.8
 
 
-int isBatteryParameter_OutOfRange(float currentInput, float minimumThreshold, float maximumThreshold) {
+bool isBatteryParameter_OutOfRange(float currentInput, float minimumThreshold, float maximumThreshold) {
     return ((currentInput < minimumThreshold) || (currentInput > maximumThreshold));
 }
 
-int isTemperatureWithinRange(float currentTemperature) {
+bool isTemperatureWithinRange(float currentTemperature) {
   int TemperatureStatus = isBatteryParameter_OutOfRange(currentTemperature, (float)MINIMUM_TEMPERATURE, (float)MAXIMUM_TEMPERATURE);
   if(TemperatureStatus)
     printf("Temperature out of range!\n");
   return TemperatureStatus;
 }
 
-int isSOCWithinRange(float currentSOC) {
+bool isSOCWithinRange(float currentSOC) {
   int SOCStatus = isBatteryParameter_OutOfRange(currentSOC, (float)MINIMUM_SOC, (float)MAXIMUM_SOC);
   if(SOCStatus)
       printf("State of Charge out of range!\n");
   return SOCStatus;
 }
 
-int isChargeRateWithinRange(float currentChargeRate) {
+bool isChargeRateWithinRange(float currentChargeRate) {
   int ChargeRateStatus = isBatteryParameter_OutOfRange(currentChargeRate, (float)MINIMUM_CHARGERATE, (float)MAXIMUM_CHARGERATE);
   if(ChargeRateStatus)
         printf("Charge Rate out of range!\n");
   return ChargeRateStatus;
 }
-int BatteryIsOk(float currentTemperature, float currentSOC, float currentChargeRate) {
+bool BatteryIsOk(float currentTemperature, float currentSOC, float currentChargeRate) {
     return (isTemperatureWithinRange(currentTemperature) & isSOCWithinRange(currentSOC) & isChargeRateWithinRange(currentChargeRate));  
 }
 
-int TestBatteryIsOk(float inputTemperature, float inputSOC, float inputChargeRate){
+bool TestBatteryIsOk(float inputTemperature, float inputSOC, float inputChargeRate){
   return BatteryIsOk(inputTemperature, inputSOC, inputChargeRate);
 }
 
