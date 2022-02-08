b8ba80572e7be70f870ab67d87a4d2a60b45cd57
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-02-08 10:50:47 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -2,12 +2,16 @@
 #include <assert.h>
 #include <stdbool.h>
 
-#define MINIMUM_TEMPERATURE    0.0
-#define MAXIMUM_TEMPERATURE    45.0
 #define MINIMUM_SOC    20.0
 #define MAXIMUM_SOC    80.0
 #define MINIMUM_CHARGERATE    0
 #define MAXIMUM_CHARGERATE    0.8
+
+void setRangeforTemperature(float low, float high)
+{
+	#define MINIMUM_TEMPERATURE low
+	#define MAXIMUM_TEMPERATURE high
+}
 
 void printOnDisplay(char *stringToBePrinted) {
     printf("%s \n", stringToBePrinted);
@@ -47,6 +51,7 @@ void TestBatteryIsOk(bool expectedOutput, float inputTemperature, float inputSOC
 }
 
 int main() {
+    setRangeforTemperature(0.0,45.0);
   TestBatteryIsOk(0,25, 70, 0.7);
   TestBatteryIsOk(1,50, 85, 0);
 }
